433137cc25eeeabf402d773cd65650c99cc7360d
Victor Martinez <vic.vazquez.0298@gmail.com>
2023-12-06 18:06:32 -0600

    Api Rest

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once "controladores/rutas.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 
 require_once "modelos/usuarios.modelo.php";
-require_once "modelos/cursos.modelo.php";
 
 $rutas = new ControladorRutas();
 $rutas->index();
